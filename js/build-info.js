08333cdb6093dbe047cde609e0db5bbdda7726f5
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-04-22 12:44:39 -0300

    Add files via upload

--- js/build-info.js
+++ js/build-info.js
@@ -1,7 +1,6 @@
 export const BUILD_INFO = {
-  version: 'v0.1.0',
+  version: 'v0.1.1',
   buildDate: '2026-04-22',
-  buildTime: '13:02 UTC',
-  module: 'São Paulo',
-  packageName: 'central190_v0.1.0_2026-04-22_13h02_build.zip'
+  buildTime: '14:34 UTC',
+  module: 'São Paulo'
 };
